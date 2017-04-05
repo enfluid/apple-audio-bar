99a399542d8cef33c810132e249cbd29ea2cdeac
Rudolf Adamkovič <salutis@me.com>
2017-04-05 10:39:28 +0200

    Convert remaining unit tests

--- AudioBarCore/ReadyToPlayStateTests.swift
+++ AudioBarCore/ReadyToPlayStateTests.swift
@@ -21,278 +21,429 @@
 // SOFTWARE.
 
 import XCTest
+
 @testable import AudioBarCore
 
-final class ReadyToPlayStateTests: XCTestCase {}
+final class ReadyToPlayStateTests: XCTestCase {
+
+    public var state = ReadyToPlayState()
+
+}
+
+extension ReadyToPlayStateTests {
+
+    func testOnPlayerDidUpdateCurrentTime() {
+        state.currentTime = .foo
+        state.onPlayerDidUpdateCurrentTime(.bar)
+        expect(state.currentTime, equals: .bar)
+    }
+
+    func testOnPlayerDidEnd() {
+        state.currentTime = .foo
+        state.info = .init(duration: .bar)
+        state.onPlayerDidPlayToEnd()
+        expect(state.isPlaying, equals: false)
+        expect(state.currentTime, equals: .bar)
+    }
+
+}
+
+extension ReadyToPlayStateTests {
+
+    func testOnUserDidTapPlaybutton() throws {
+        state.isPlaying = false
+        let mock = injectMock(into: state.world)
+        mock.expect(Player.PlayAction())
+        try state.onUserDidTapPlayButton()
+        expect(state.isPlaying, equals: true)
+    }
+
+    func testOnUserDidTapPauseButton() throws {
+        state.isPlaying = true
+        let mock = injectMock(into: state.world)
+        mock.expect(Player.PauseAction())
+        try state.onUserDidTapPauseButton()
+        expect(state.isPlaying, equals: false)
+    }
+
+}
+
+extension ReadyToPlayStateTests {
+
+    func testOnUserDidTapSeekBackButton1() throws {
+        state.currentTime = ReadyToPlayState.seekInterval + 1
+        let mock = injectMock(into: state.world)
+        mock.expect(Player.SeekAction(currentTime: 1))
+        try state.onUserDidTapSeekBackButton()
+        expect(state.currentTime, equals: 1)
+    }
+
+    func testOnUserDidTapSeekBackButton2() throws {
+        state.currentTime = ReadyToPlayState.seekInterval + 2
+        let mock = injectMock(into: state.world)
+        mock.expect(Player.SeekAction(currentTime: 2))
+        try state.onUserDidTapSeekBackButton()
+        expect(state.currentTime, equals: 2)
+    }
+
+    func testOnUserDidTapSeekBackButton3() throws {
+        state.currentTime = ReadyToPlayState.seekInterval
+        let mock = injectMock(into: state.world)
+        mock.expect(Player.SeekAction(currentTime: 0))
+        try state.onUserDidTapSeekBackButton()
+        expect(state.currentTime, equals: 0)
+    }
+
+    func testOnUserDidTapSeekBackButton4() throws {
+        state.currentTime = ReadyToPlayState.seekInterval - 1
+        let mock = injectMock(into: state.world)
+        mock.expect(Player.SeekAction(currentTime: 0))
+        try state.onUserDidTapSeekBackButton()
+        expect(state.currentTime, equals: 0)
+    }
+
+    func testOnUserDidTapSeekBackButton5() throws {
+        state.currentTime = ReadyToPlayState.seekInterval - 2
+        let mock = injectMock(into: state.world)
+        mock.expect(Player.SeekAction(currentTime: 0))
+        try state.onUserDidTapSeekBackButton()
+        expect(state.currentTime, equals: 0)
+    }
+
+}
+
+extension ReadyToPlayStateTests {
+
+    func testOnUserDidTapSeekForwardButton1() throws {
+        state.currentTime = 0
+        state.info = .init(duration: .infinity)
+        let mock = injectMock(into: state.world)
+        mock.expect(Player.SeekAction(currentTime: 0 + ReadyToPlayState.seekInterval))
+        try state.onUserDidTapSeekForwardButton()
+        expect(state.currentTime, equals: 0 + ReadyToPlayState.seekInterval)
+    }
+
+    func testOnUserDidTapSeekForwardButton2() throws {
+        state.currentTime = 1
+        state.info = .init(duration: .infinity)
+        let mock = injectMock(into: state.world)
+        mock.expect(Player.SeekAction(currentTime: 1 + ReadyToPlayState.seekInterval))
+        try state.onUserDidTapSeekForwardButton()
+        expect(state.currentTime, equals: 1 + ReadyToPlayState.seekInterval)
+    }
+
+    func testOnUserDidTapSeekForwardButton3() throws {
+        precondition(ReadyToPlayState.seekInterval < 60)
+        state.currentTime = 60 - 1
+        state.info = .init(duration: 60)
+        let mock = injectMock(into: state.world)
+        mock.expect(Player.SeekAction(currentTime: 60))
+        try state.onUserDidTapSeekForwardButton()
+        expect(state.currentTime, equals: 60)
+    }
+
+    func testOnUserDidTapSeekForwardButton4() throws {
+        precondition(ReadyToPlayState.seekInterval < 60)
+        state.currentTime = 60 - 2
+        state.info = .init(duration: 60)
+        let mock = injectMock(into: state.world)
+        mock.expect(Player.SeekAction(currentTime: 60))
+        try state.onUserDidTapSeekForwardButton()
+        expect(state.currentTime, equals: 60)
+    }
+
+}
+
+extension ReadyToPlayStateTests {
+
+    func testReset() throws {
+        let mock = injectMock(into: state.world)
+        mock.expect(Player.LoadAction(url: nil))
+        try state.reset()
+        expect(state.nextState, equals: WaitingForURLState())
+    }
+
+}
 
 extension ReadyToPlayStateTests {
 
     func testViewPlayPauseButtonImage1() {
-        let state = ReadyToPlayState(isPlaying: true)
+        state.isPlaying = true
         let view = state.present() as! AudioBarView
         expect(view.playPauseButtonImage, equals: .pause)
     }
 
     func testViewPlayPauseButtonImage2() {
-        let state = ReadyToPlayState(isPlaying: false)
+        state.isPlaying = false
         let view = state.present() as! AudioBarView
         expect(view.playPauseButtonImage, equals: .play)
     }
 
     func testViewPlaybackTime1() {
-        let state = ReadyToPlayState(currentTime: nil)
+        state.currentTime = nil
         let view = state.present() as! AudioBarView
         expect(view.playbackTime, equals: "")
     }
 
     func testViewPlaybackTime2() {
-        let state = ReadyToPlayState(currentTime: 0, info: .init(duration: 1))
+        state.currentTime = 0
+        state.info = .init(duration: 1)
         let view = state.present() as! AudioBarView
         expect(view.playbackTime, equals: "-0:01")
     }
 
     func testViewPlaybackTime3() {
-        let state = ReadyToPlayState(currentTime: 0, info: .init(duration: 61))
+        state.currentTime = 0
+        state.info = .init(duration: 61)
         let view = state.present() as! AudioBarView
         expect(view.playbackTime, equals: "-1:01")
     }
 
     func testViewPlaybackTime4() {
-        let state = ReadyToPlayState(currentTime: 20, info: .init(duration: 60))
+        state.currentTime = 20
+        state.info = .init(duration: 60)
         let view = state.present() as! AudioBarView
         expect(view.playbackTime, equals: "-0:40")
     }
 
-    func testViewPlaybackDuration1() {
-        let state = ReadyToPlayState(info: .init(duration: 1))
+    func testViewPlaybackDuration() {
+        state.info = .init(duration: .foo)
         let view = state.present() as! AudioBarView
-        expect(view.playbackDuration, equals: 1)
-    }
-
-    func testViewPlaybackDuration2() {
-        let state = ReadyToPlayState(info: .init(duration: 2))
-        let view = state.present() as! AudioBarView
-        expect(view.playbackDuration, equals: 2)
+        expect(view.playbackDuration, equals: .foo)
     }
 
     func testViewElapsedPlaybackTime1() {
-        let state = ReadyToPlayState(currentTime: nil)
+        state.currentTime = nil
         let view = state.present() as! AudioBarView
         expect(view.elapsedPlaybackTime, equals: 0)
     }
 
     func testViewElapsedPlaybackTime2() {
-        let state = ReadyToPlayState(currentTime: 1)
+        state.currentTime = .foo
         let view = state.present() as! AudioBarView
-        expect(view.elapsedPlaybackTime, equals: 1)
-    }
-
-    func testViewElapsedPlaybackTime3() {
-        let state = ReadyToPlayState(currentTime: 2)
-        let view = state.present() as! AudioBarView
-        expect(view.elapsedPlaybackTime, equals: 2)
+        expect(view.elapsedPlaybackTime, equals: .foo)
     }
 
     func testViewSeekIntervalWhenReadyToPlay() {
-        let state = ReadyToPlayState()
         let view = state.present() as! AudioBarView
         expect(view.seekInterval, equals: 15)
     }
 
     func testViewIsPlayCommandEnabled1() {
-        let state = ReadyToPlayState(isPlaying: true)
+        state.isPlaying = true
         let view = state.present() as! AudioBarView
         expect(view.isPlayCommandEnabled, equals: false)
     }
 
     func testViewIsPlayCommandEnabled2() {
-        let state = ReadyToPlayState(isPlaying: false, currentTime: 0, info: .init(duration: 1))
+        state.isPlaying = false
+        state.currentTime = 0
+        state.info = .init(duration: 1)
         let view = state.present() as! AudioBarView
         expect(view.isPlayCommandEnabled, equals: true)
     }
 
     func testViewIsPlayCommandEnabled3() {
-        let state = ReadyToPlayState(isPlaying: false, currentTime: 1, info: .init(duration: 1))
+        state.isPlaying = false
+        state.currentTime = 1
+        state.info = .init(duration: 1)
         let view = state.present() as! AudioBarView
         expect(view.isPlayCommandEnabled, equals: false)
     }
 
     func testViewIsPauseCommandEnabled1() {
-        let state = ReadyToPlayState(isPlaying: false)
+        state.isPlaying = false
         let view = state.present() as! AudioBarView
         expect(view.isPauseCommandEnabled, equals: false)
     }
 
     func testViewIsPauseCommandEnabled2() {
-        let state = ReadyToPlayState(isPlaying: true, currentTime: 0, info: .init(duration: 1))
+        state.isPlaying = true
+        state.currentTime = 0
+        state.info = .init(duration: 1)
         let view = state.present() as! AudioBarView
         expect(view.isPauseCommandEnabled, equals: true)
     }
 
     func testViewIsPauseCommandEnabled3() {
-        let state = ReadyToPlayState(isPlaying: true, currentTime: 1, info: .init(duration: 1))
+        state.isPlaying = true
+        state.currentTime = 1
+        state.info = .init(duration: 1)
         let view = state.present() as! AudioBarView
         expect(view.isPauseCommandEnabled, equals: false)
     }
 
     func testViewTrackName1() {
-        let state = ReadyToPlayState(info: .init(title: nil))
+        state.info = .init(title: nil)
         let view = state.present() as! AudioBarView
         expect(view.trackName, equals: nil)
     }
 
     func testViewTrackName2() {
-        let state = ReadyToPlayState(info: .init(title: "1"))
+        state.info = .init(title: "A")
         let view = state.present() as! AudioBarView
-        expect(view.trackName, equals: "1")
+        expect(view.trackName, equals: "A")
     }
 
     func testViewTrackName3() {
-        let state = ReadyToPlayState(info: .init(title: "2"))
+        state.info = .init(title: "B")
         let view = state.present() as! AudioBarView
-        expect(view.trackName, equals: "2")
+        expect(view.trackName, equals: "B")
     }
 
     func testViewArtistName1() {
-        let state = ReadyToPlayState(info: .init(artist: nil))
+        state.info = .init(artist: nil)
         let view = state.present() as! AudioBarView
         expect(view.artistName, equals: nil)
     }
 
     func testViewArtistName2() {
-        let state = ReadyToPlayState(info: .init(artist: "3"))
+        state.info = .init(artist: "A")
         let view = state.present() as! AudioBarView
-        expect(view.artistName, equals: "3")
+        expect(view.artistName, equals: "A")
     }
 
     func testViewArtistName3() {
-        let state = ReadyToPlayState(info: .init(artist: "4"))
+        state.info = .init(artist: "B")
         let view = state.present() as! AudioBarView
-        expect(view.artistName, equals: "4")
+        expect(view.artistName, equals: "B")
     }
 
     func testViewAlbumName1() {
-        let state = ReadyToPlayState(info: .init(album: nil))
+        state.info = .init(album: nil)
         let view = state.present() as! AudioBarView
         expect(view.albumName, equals: nil)
     }
 
     func testViewAlbumName2() {
-        let state = ReadyToPlayState(info: .init(album: "5"))
+        state.info = .init(album: "A")
         let view = state.present() as! AudioBarView
-        expect(view.albumName, equals: "5")
+        expect(view.albumName, equals: "A")
     }
 
     func testViewAlbumName3() {
-        let state = ReadyToPlayState(info: .init(album: "6"))
+        state.info = .init(album: "B")
         let view = state.present() as! AudioBarView
-        expect(view.albumName, equals: "6")
+        expect(view.albumName, equals: "B")
     }
 
     func testViewArtworkData1() {
-        let state = ReadyToPlayState(info: .init(artwork: nil))
+        state.info = .init(artwork: nil)
         let view = state.present() as! AudioBarView
         expect(view.artworkData, equals: nil)
     }
 
     func testViewArtworkData2() {
-        let state = ReadyToPlayState(info: .init(artwork: Data()))
+        state.info = .init(artwork: .init())
         let view = state.present() as! AudioBarView
-        expect(view.artworkData, equals: Data())
+        expect(view.artworkData, equals: .init())
     }
 
     func testViewIsLoadingIndicatorVisible1() {
-        let state = ReadyToPlayState(isPlaying: true, currentTime: nil)
+        state.isPlaying = true
+        state.currentTime = nil
         let view = state.present() as! AudioBarView
         expect(view.isLoadingIndicatorVisible, equals: true)
     }
 
     func testViewIsLoadingIndicatorVisible2() {
-        let state = ReadyToPlayState(isPlaying: false, currentTime: nil)
+        state.isPlaying = false
+        state.currentTime = nil
         let view = state.present() as! AudioBarView
         expect(view.isLoadingIndicatorVisible, equals: false)
     }
 
     func testViewIsLoadingIndicatorVisible3() {
-        let state = ReadyToPlayState(isPlaying: true, currentTime: 0)
+        state.isPlaying = true
+        state.currentTime = 0
         let view = state.present() as! AudioBarView
         expect(view.isLoadingIndicatorVisible, equals: false)
     }
 
     func testViewIsLoadingIndicatorVisible4() {
-        let state = ReadyToPlayState(isPlaying: false, currentTime: 0)
+        state.isPlaying = false
+        state.currentTime = 0
         let view = state.present() as! AudioBarView
         expect(view.isLoadingIndicatorVisible, equals: false)
     }
 
     func testViewIsSeekBackButtonEnabled1() {
-        let state = ReadyToPlayState(currentTime: nil)
+        state.currentTime = nil
         let view = state.present() as! AudioBarView
         expect(view.isSeekBackButtonEnabled, equals: false)
     }
 
     func testViewIsSeekBackButtonEnabled2() {
-        let state = ReadyToPlayState(currentTime: 0, info: .init(duration: 60))
+        state.currentTime = 0
+        state.info = .init(duration: 60)
         let view = state.present() as! AudioBarView
         expect(view.isSeekBackButtonEnabled, equals: false)
     }
 
     func testViewIsSeekBackButtonEnabled3() {
-        let state = ReadyToPlayState(currentTime: 1, info: .init(duration: 60))
+        state.currentTime = 1
+        state.info = .init(duration: 60)
         let view = state.present() as! AudioBarView
         expect(view.isSeekBackButtonEnabled, equals: true)
     }
 
     func testViewIsSeekBackButtonEnabled4() {
-        let state = ReadyToPlayState(currentTime: 2, info: .init(duration: 60))
+        state.currentTime = 2
+        state.info = .init(duration: 60)
         let view = state.present() as! AudioBarView
         expect(view.isSeekBackButtonEnabled, equals: true)
     }
 
     func testViewIsSeekForwardButtonEnabled1() {
-        let state = ReadyToPlayState(currentTime: nil)
+        state.currentTime = nil
         let view = state.present() as! AudioBarView
         expect(view.isSeekForwardButtonEnabled, equals: false)
     }
 
     func testViewIsSeekForwardButtonEnabled2() {
-        let state = ReadyToPlayState(currentTime: 58, info: .init(duration: 60))
+        state.currentTime = 59
+        state.info = .init(duration: 60)
         let view = state.present() as! AudioBarView
         expect(view.isSeekForwardButtonEnabled, equals: true)
     }
 
     func testViewIsSeekForwardButtonEnabled3() {
-        let state = ReadyToPlayState(currentTime: 59, info: .init(duration: 60))
+        state.currentTime = 119
+        state.info = .init(duration: 120)
         let view = state.present() as! AudioBarView
         expect(view.isSeekForwardButtonEnabled, equals: true)
     }
 
     func testViewIsSeekForwardButtonEnabled4() {
-        let state = ReadyToPlayState(currentTime: 60, info: .init(duration: 60))
+        state.currentTime = 60
+        state.info = .init(duration: 60)
         let view = state.present() as! AudioBarView
         expect(view.isSeekForwardButtonEnabled, equals: false)
     }
 
     func testViewIsPlayPauseButtonEnabled1() {
-        let state = ReadyToPlayState(currentTime: nil)
+        state.currentTime = nil
         let view = state.present() as! AudioBarView
         expect(view.isPlayPauseButtonEnabled, equals: true)
     }
 
     func testViewIsPlayPauseButtonEnabled2() {
-        let state = ReadyToPlayState(currentTime: 60, info: .init(duration: 60))
+        state.currentTime = 60
+        state.info = .init(duration: 60)
         let view = state.present() as! AudioBarView
         expect(view.isPlayPauseButtonEnabled, equals: false)
     }
 
     func testViewIsPlayPauseButtonEnabled3() {
-        let state = ReadyToPlayState(currentTime: 59, info: .init(duration: 60))
+        state.currentTime = 59
+        state.info = .init(duration: 60)
         let view = state.present() as! AudioBarView
         expect(view.isPlayPauseButtonEnabled, equals: true)
     }
 
     func testViewIsPlayPauseButtonEnabled4() {
-        let state = ReadyToPlayState(currentTime: 58, info: .init(duration: 60))
+        state.currentTime = 119
+        state.info = .init(duration: 120)
         let view = state.present() as! AudioBarView
         expect(view.isPlayPauseButtonEnabled, equals: true)
     }
